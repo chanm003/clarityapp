3df88aa601247697f257b654f91fe47e02927772
Mike Chan <mike.chan@kmdev.local>
2017-07-18 18:51:36 +0200

    Skeleton Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ClarityModule } from 'clarity-angular';
+import { UiModule } from './ui/ui.module';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    ClarityModule.forRoot()
+    ClarityModule.forRoot(),
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
